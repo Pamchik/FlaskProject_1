78ef494e70303495b7ee1fc860c3e1a075a8dae5
Alexander <s.pamchik@ya.ru>
2022-12-05 00:11:41 +0600

    Update site

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+from collections import namedtuple
+
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FlaskProject_1.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Assortment.db'
+
+app.config['SQLALCHEMY_TRACK_MODFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Things(db.Model):
+list = namedtuple('list', 'title price')
+list_all = []
+
+
+class Assortment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -22,6 +30,14 @@ class Things(db.Model):
 #  3    name3   00.00   text3         dd.mm.yyyy   False
 #  ...
 
+
+# class Basket_user(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.Float, nullable=False)
+#     description = db.Column(db.Text(500), nullable=False)
+
+
     def __repr__(self):
         return self.id
 
@@ -29,13 +45,35 @@ class Things(db.Model):
 @app.route('/')
 @app.route('/home')
 def index():
-    items = Things.query.order_by(Things.price).all()
-    return render_template("index.html", items=items)
+     items = Assortment.query.order_by(Assortment.price).all()
+     return render_template("index.html", items=items)
+
+
+@app.route("/basket-add/<int:id>", methods=['POST', 'GET'])
+def basket_add(id):
+    item = Assortment.query.get(id)
+
+#    if request.method == 'POST':
+#
+    ititle = item.title
+    iprice = item.price
+#
+    try:
+        list_all.append(list(ititle, iprice))
+        print(list_all)
+        return redirect(url_for('index'))
+    except:
+        return "При добавлении товара в корзину произошла ошибка"
+#    else:
+
+    #return render_template("index.html")
+
+    return redirect("/")
 
 
 @app.route('/list-things')
 def list_things():
-    items = Things.query.order_by(Things.price).all()
+    items = Assortment.query.order_by(Assortment.price).all()
     return render_template("list_things.html", items=items)
 
 
@@ -44,15 +82,20 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/basket')
+def basket():
+    return render_template("basket.html", list_all=list_all)
+
+
 @app.route('/list-things/<int:id>')
 def list_detail(id):
-    item = Things.query.get(id)
+    item = Assortment.query.get(id)
     return render_template("item_detail.html", item=item)
 
 
 @app.route('/list-things/<int:id>/delete')
 def list_delete(id):
-    item = Things.query.get_or_404(id)
+    item = Assortment.query.get_or_404(id)
 
     try:
         db.session.delete(item)
@@ -64,7 +107,7 @@ def list_delete(id):
 
 @app.route('/list-things/<int:id>/update', methods=['POST', 'GET'])
 def list_update(id):
-    item = Things.query.get(id)
+    item = Assortment.query.get(id)
     if request.method == 'POST':
         item.title = request.form['title']
         item.price = request.form['price']
@@ -79,7 +122,6 @@ def list_update(id):
         return render_template("item_update.html", item=item)
 
 
-
 @app.route('/create-item', methods=['POST', 'GET'])
 def create_item():
     if request.method == 'POST':
@@ -87,7 +129,7 @@ def create_item():
         price = request.form['price']
         description = request.form['description']
 
-        thing = Things(title=title, price=price, description=description)
+        thing = Assortment(title=title, price=price, description=description)
 
         try:
             db.session.add(thing)
@@ -103,7 +145,7 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-#from app import app, db
+#from main import app, db
 #app.app_context().push()
 #db.create_all()
 
